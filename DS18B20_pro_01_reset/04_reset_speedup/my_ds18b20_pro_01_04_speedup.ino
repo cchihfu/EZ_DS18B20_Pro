123b9d9dcd18c72012392f189b23e0e23a9fcfed
cchihfu <cchihfu@gmail.com>
2017-11-14 11:15:47 +0800

    test Tx Rx mintime

--- DS18B20_pro_01_reset/04_reset_speedup/my_ds18b20_pro_01_04_speedup.ino
+++ DS18B20_pro_01_reset/04_reset_speedup/my_ds18b20_pro_01_04_speedup.ino
@@ -8,51 +8,98 @@
 //Arduino數位腳位2接到DS18B20的第2腳(DQ)，DQ為Data input/output的縮寫
 const uint8_t g_dq_pin =2;
 
-uint16_t tx_low_time=720;
-uint16_t rx_wait_read_time=70;
-uint16_t rx_pass_time=410;
+uint16_t g_time_keep_low=720;
+uint16_t g_time_wait_read=70;
+uint16_t g_time_through=410;
+uint16_t g_time_keep_low_min=0;
+uint16_t g_time_wait_read_min=0;
+
 
 void setup() {
 	Serial.begin(9600);
 	Serial.println("<<Star Test the DS18B20 >>");
-
 }
 
 void loop() {
 	//先測Tx之時間min
+	TestTxTime();
 
-	//Serial.println("<<Test the Speed : Tx Stay Low Time >>");
-	uint8_t test_num=1;
-	uint8_t tx_low_time_min=0;
+	//先測Tx之時間min
+	TestRxTime();
 
-	tx_low_time=1;
-	for(int i = 0; i < 100; ++i)
-	{
-		while(!IsResetOk()) {
-			tx_low_time++;
-		}
-		if(tx_low_time>tx_low_time_max)
-		{
-			tx_low_time_max=tx_low_time;
-		}
-		/*Serial.print("the ");
-		Serial.print(test_num);
-		Serial.print(" times test: ");
-		PrintInfo(IsResetOk()) ;*/
+	//使用min值再加一些作測試10次
+	TestMinTime();
 
-		test_num++;
-	}
-	Serial.print("the 100 times test result:Tx stay in LOW time >>");
-	Serial.println(tx_low_time_max);
-	//PrintInfo(IsResetOk()) ;
-	delay(1000);
+	Serial.println("");
+	delay(3000);
 }
 
-uint8_t IsResetOk()
+void TestTxTime()
+{
+	g_time_keep_low=1;
+	for(int i = 0; i < 100; ++i)
+	{
+		while(!CommandReset()) {
+			g_time_keep_low++;
+		}
+		if(g_time_keep_low>g_time_keep_low_min)
+		{
+			g_time_keep_low_min=g_time_keep_low;
+		}
+
+	}
+	Serial.print("the 100 times test result:Tx stay in LOW time >>");
+	Serial.println(g_time_keep_low_min);
+
+}
+void TestRxTime()
+{
+	//將Tx維持低電位的基礎設定值取回來
+	g_time_keep_low=720;
+	g_time_wait_read=1;
+	for(int i = 0; i < 100; ++i)
+	{
+		while(!CommandReset()) {
+			//無法reset成功就增加等待取時間
+			g_time_wait_read++;
+
+			//改變Rx穿越等待時段
+			g_time_through=480-g_time_wait_read;
+		}
+		//成功時就將最小值放入其中
+		if(g_time_wait_read>g_time_wait_read_min)
+		{
+			g_time_wait_read_min=g_time_wait_read;
+		}
+	}
+	Serial.print("the 100 times test result:Rx wait time >>");
+	Serial.println(g_time_wait_read_min);
+}
+
+void TestMinTime()
+{
+	//Tx維持低電位的時間以最小值加30
+	g_time_keep_low=g_time_keep_low_min+30;
+
+	//Rx讀取時間以最小值加3
+	g_time_wait_read=g_time_wait_read_min+3;
+
+	//調整Rx穿越時間
+	g_time_through=480-g_time_wait_read;
+
+	//測試十次
+	for(int i = 0; i < 10; ++i)
+	{
+		ShowOkInfo(CommandReset()) ;
+	}
+}
+
+
+uint8_t CommandReset()
 {
 	TxReset();
-	uint8_t f=RxReply();
-	PassRx();
+	uint8_t f=RxResult();
+	ThroughRx();
 	return f;
 }
 
@@ -62,30 +109,30 @@ void TxReset() {
 	//Tx階段：Step 2.主機拉低電位
 	digitalWrite(g_dq_pin, LOW);
 	//Tx階段：Step 3.主機持續於低電位
-	delayMicroseconds(tx_low_time);
+	delayMicroseconds(g_time_keep_low);
 	//Tx階段：Step 4.主機釋放電位控制，轉為輸入狀態
 	pinMode(g_dq_pin, INPUT);
 }
-uint8_t RxReply() {
+uint8_t RxResult() {
 	//Rx階段：Step 5.延時並讀取DS18B20回應電位值
-	delayMicroseconds(rx_wait_read_time);
+	delayMicroseconds(g_time_wait_read);
 	return !digitalRead(g_dq_pin);
 }
-void PassRx() {
+void ThroughRx() {
 	//Rx階段：Step 6.延時並讓其超過Rx的480us時間
-	delayMicroseconds(rx_pass_time);
+	delayMicroseconds(g_time_through);
 }
 
-void PrintInfo(uint8_t isOk)
+void ShowOkInfo(uint8_t isOk)
 {
 	if(isOk) {
 		Serial.print("Reset OK");
 		Serial.print(",");
-		Serial.print(tx_low_time);
+		Serial.print(g_time_keep_low);
 		Serial.print(",");
-		Serial.print(rx_wait_read_time);
+		Serial.print(g_time_wait_read);
 		Serial.print(",");
-		Serial.println(rx_pass_time);
+		Serial.println(g_time_through);
 	} else {
 		Serial.println("Reset Fail");
 	}
